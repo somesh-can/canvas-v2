432e9f8fa7ad589f18954c59df98c29b6999aea6
somesh-can <k.s.somesh11@gmail.com>
2026-04-16 14:32:12 +0200

    Simplify prototype to static Vite app

--- react-router.config.ts
+++
@@ -1,8 +0,0 @@
-import type { Config } from '@react-router/dev/config';
-
-export default {
-	appDirectory: './src/app',
-	// Static deployment target for Vercel.
-	ssr: false,
-	prerender: ['/*?'],
-} satisfies Config;
